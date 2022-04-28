6cf3f38cb2329b26380ed425381309a6075e9203
jsolarz <jsolarz@google.com>
2022-04-28 18:03:22 +0200

    Fixed spelling

--- 2_repo-change.sh
+++ 2_repo-change.sh
@@ -22,7 +22,7 @@ if [ ! -d "$REPO_NAME" ]; then
 fi
 
 # watch ClusterRolebinding
-pe "kubectl get rolebindings pod-creators -o yaml"
+pe "kubectl get rolebindings -n bank-of-anthos pod-creators -o yaml"
 
 # (in another termin) edit
 pe "vim ${REPO_NAME}/namespace/bank-of-anthos/pod-creator-rolebinding.yaml"
@@ -32,15 +32,15 @@ pe "vim ${REPO_NAME}/namespace/bank-of-anthos/pod-creator-rolebinding.yaml"
 #  name: developer2@bank-of-anthos.com
 #  apiGroup: rbac.authorization.k8s.io
 
-pe "kubectl get rolebindings pod-creators -o yaml"
+pe "kubectl get rolebindings -n bank-of-anthos pod-creators -o yaml"
 
 pe "cd ${REPO_NAME}"
 # commit changes
-pe "git add namespace/bank-of-anthos/pod-creator-rolebinding.yaml"
+pe "git add namespaces/bank-of-anthos/pod-creator-rolebinding.yaml"
 pe "git commit -m 'Add developer2 to pod-creator'"
 pe "git push"
 
-pe "kubectl get rolebindings pod-creator -o yaml"
+pe "kubectl get rolebindings -n bank-of-anthos pod-creator -o yaml"
 
 pe "export HASH=$(git rev-parse HEAD)"
 
@@ -48,4 +48,4 @@ pe "export HASH=$(git rev-parse HEAD)"
 pe "git revert $HASH"
 pe "git push"
 
-pe "kubectl get clusterrolebindings namespace-readers -o yaml"
+pe "kubectl get rolebindings -n bank-of-anthos pod-creator -o yaml"
