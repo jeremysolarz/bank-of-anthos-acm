44159f180d2375eecfeec54276560c52882eece5
jsolarz <jsolarz@google.com>
2022-04-28 18:14:25 +0200

    Use sed insead of vi

--- 2_repo-change.sh
+++ 2_repo-change.sh
@@ -16,16 +16,17 @@ export BASE_DIR=$(realpath $(dirname $BASH_SOURCE))
 ######################################################
 # Change in repo
 ######################################################
-if [ ! -d "$REPO_NAME" ]; then
+# if [ ! -d "$REPO_NAME" ]; then
   # clone repo
-  git clone git@github.com:${GIT_USER}/${REPO_NAME}.git
-fi
+rm -rf ${REPO_NAME}
+git clone git@github.com:${GIT_USER}/${REPO_NAME}.git > /dev/null 2>&1
+# fi
 
 # watch ClusterRolebinding
 pe "kubectl get rolebindings -n bank-of-anthos pod-creators -o yaml"
 
 # (in another termin) edit
-pe "vim ${REPO_NAME}/namespaces/bank-of-anthos/pod-creator-rolebinding.yaml"
+pe "sed '/#insert/r dev2.txt' ${REPO_NAME}/namespaces/bank-of-anthos/pod-creator-rolebinding.yaml"
 
 # add to subjects
 # - kind: User
